77bbaa696943353f985c2bbf6b2fc67c074b0b06
HiMrHu <454053152@qq.com>
2017-09-12 13:53:29 +0800

    更新了一些小bug优化逻辑

--- Buttons/Button.jsx
+++ Buttons/Button.jsx
@@ -1,24 +1,43 @@
 /**
  * @author 
  * @file 按钮
- * @param minor {props} 次要按钮，默认为蓝色主要按钮
- * @param inline {props} 行级小按钮
+ * @param type = 'inline' {props} 次要按钮，默认为蓝色主要按钮
  * @param disabled {props} 禁用按钮。
+ * @param color {props} 颜色，默认蓝色，可选minor
+ * @param onClick 给button绑定的点击函数
  */
 import React from 'react'
 import classNames from 'classnames'
 import './Button.css'
 class Button extends React.Component {
-  render () {
-    const { minor, disabled } = this.props
-    console.log(minor)
+  constructor() {
+    super()
+    this.onClick = this.onClick.bind(this)
+  }
+  onClick(e) {
+    if (this.props.disabled) {
+      e.preventDefault()
+    }
+    if (this.props.onClick) {
+      this.props.onClick(e)
+    }
+  }
+
+  render() {
+    const { type, disabled, color } = this.props
     const className = classNames(
       className,
-      'btn-inline',
-      `btn${minor ? '-minor' : ''}-color`,
+      `${type === 'inline' ? 'btn-inline' : 'btn-block'}`,
+      `btn${color === 'minor' ? 'type' : ''}-color`,
       `${disabled ? 'btn-disabled' : ''}`
     )
-    return <button className={className} disabled={disabled ? 'disabled' : ''}> 2333</button>
+    return <button
+      className={className}
+      disabled={disabled ? 'disabled' : ''}
+      onClick={this.onClick}
+    >
+      {this.props.children}
+    </button>
   }
 }
 
