3205ce28a040b0cdc2d9f9d1d4a4326ccdd1ce71
HiMrHu <454053152@qq.com>
2017-09-12 11:28:30 +0800

    button样式更新，下一步学习写动画效果

--- Buttons/Button.jsx
+++ Buttons/Button.jsx
@@ -1,24 +1,25 @@
 /**
  * @author 
  * @file 按钮
+ * @param minor {props} 次要按钮，默认为蓝色主要按钮
+ * @param inline {props} 行级小按钮
+ * @param disabled {props} 禁用按钮。
  */
-import React from 'react';
-
+import React from 'react'
+import classNames from 'classnames'
+import './Button.css'
 class Button extends React.Component {
-  constructor() {
-    super(props);
-    this.state = { 
-        
-     };
-  }
-
-  render() {
-    return <button></button>;
-  }
-
-  componentDidMount() {
-    
+  render () {
+    const { minor, disabled } = this.props
+    console.log(minor)
+    const className = classNames(
+      className,
+      'btn-inline',
+      `btn${minor ? '-minor' : ''}-color`,
+      `${disabled ? 'btn-disabled' : ''}`
+    )
+    return <button className={className} disabled={disabled ? 'disabled' : ''}> 2333</button>
   }
 }
 
-export default Button;
+export default Button
